8637070b640ffc13627e19d7a49c5956ad6621ee
Jacek Krajewski <jacekkrajewski12@wp.pl>
2020-11-14 14:42:16 +0100

    Create page features style

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import ScheduleIcon from '@material-ui/icons/Schedule';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 
 import classes from './home.module.scss'
 import Button from './../UI/button/Button'
-import result from './../../assets/icons/badanie2.png'
+import result from './../../assets/icons/badanie2.png';
+import PageFeature from './pageFeature/PageFeature'
 
 
 const Home = () => {
@@ -16,6 +20,11 @@ const Home = () => {
             <div className={classes.introRight}>
                 <img className={classes.testResultImage} src={result} alt="photo of test results"></img>
             </div>
+        </div>
+        <div className={classes.pageFeatures}>
+            <PageFeature Icon={ScheduleIcon} name="Easy to use" text="Add test resaults to be able to start wokring with data. Add more that one results to be able to compare changes over the time." />
+            <PageFeature Icon={FormatListNumberedIcon} name="Clear user interface" text="Test results are displayed in clear and easy to understand way what allowes to work with passed data in most efficient way." />
+            <PageFeature Icon={MonetizationOnIcon} name="Totally free" text="Using the page is totally free and you are garanted that it will never change." />
         </div>
     </div>
 }
